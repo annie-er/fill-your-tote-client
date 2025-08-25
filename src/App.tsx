1613dc88c4dc6e42bd022a51d4d21776ee2f0da3
Annie Rong <annierong302@gmail.com>
2025-08-25 00:19:16 -0400

    fix: remove menu visibility from select pathnames

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css'
 import Menu from './components/Menu';
 import Home from './pages/home/Home';
@@ -12,25 +12,37 @@ import PageNotFound from './pages/page-not-found/PageNotFound';
 import ProductDetail from './pages/shop/ProductDetail';
 import DrawingDetail from './pages/drawings/DrawingDetail';
 
+function AppContent() {
+  const location = useLocation();
+
+  const hideMenu =
+    location.pathname.startsWith("/drawings/") ||
+    location.pathname.startsWith("/shop/");
+
+  return (
+    <div className="app-container">
+      {!hideMenu && <Menu />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/shop/:identifier" element={<ProductDetail />} />
+        <Route path="/drawings" element={<Drawings />} />
+        <Route path="/drawings/:identifier" element={<DrawingDetail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <Menu />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/shop/:identifier" element={<ProductDetail />} />
-          <Route path="/drawings" element={<Drawings />} />
-          <Route path="/drawings/:identifier" element={<DrawingDetail />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   )
 }
 
-export default App
+export default App;
