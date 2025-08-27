6dcea731c09ce09861fbf4a4e1394cbb02575fe8
Annie Rong <annierong302@gmail.com>
2025-08-27 12:48:42 -0400

    feat: add about page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -4,22 +4,26 @@ import "./About.css";
 const About: React.FC = () => {
   return (
     <div className="about-container">
-      <div className="about-image">
-        <img src="../images/profile.jpg" alt="About me" />
-      </div>
-      <div className="about-text">
-        <h1>Annie Rong</h1>
-        <p>
-          Annie Rong is a second-year student studying Computer Science at the University of Toronto. She is based in Richmond Hill, creating visual stories
-          on tote bags in her free time.  
-
-          She launched Fill your tote! in June of 2023, in hopes of designing creative and fun tote bags for people to wear! Her work revolves around friendship, 
-          human emotion, and love. 
-
-          Selected clients: Richmond Hill Fire Services, Cozy Grotto, Mr. Surprise.  
-
-          Unimportant note: Annie is a hopeless cat lover... 
-        </p>
+      <div className="about-title">My story</div>
+      <div className="about-content">
+        <div className="about-image">
+          <img src="/assets/me.png" alt="Me" />
+        </div>
+        <div className="about-text">
+          <h1>Hi!</h1>
+          <p>
+            I'm Annie, a second-year student studying Computer Science at the University of Toronto. I enjoy creating visual stories on tote bags in my free time.
+          </p>
+          
+          <p>
+            I launched Fill your tote! in June of 2023 through Summer Company, in hopes of designing creative and fun tote bags for people to wear!
+            My work revolves around friendship, human emotion, and love.
+          </p>
+          
+          <p>
+            I'm always looking for new ideas and collaborations, so reach out if you have anything in mind.
+          </p>
+        </div>
       </div>
     </div>
   );
